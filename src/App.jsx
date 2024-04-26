10bb7edcfba10e29298c030ff735d037bfd3a930
ndjerrou <n.djerrou@gmail.com>
2024-04-26 16:26:00 +0100

    end of correction

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,24 @@ import ExpenseList from './expense-tracker/components/ExpenseList';
 
 function App() {
   const [expenses, setExpenses] = useState([]);
-  const [selectedCategeory, setSelectedCategeory] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
 
   const addExpense = newExpense => {
-    setExpenses([...expenses, newExpense]);
+    setExpenses([...expenses, { ...newExpense, id: expenses.length + 1 }]);
   };
 
   const deleteExpense = id =>
     setExpenses(expenses.filter(expense => expense.id !== id));
 
-  const visibleExpenses = selectedCategeory
-    ? expenses.filter(expense => expense.category === selectedCategeory)
+  const visibleExpenses = selectedCategory
+    ? expenses.filter(expense => expense.category === selectedCategory)
     : expenses;
 
   return (
     <>
       <ExpenseForm onSubmit={addExpense} />
       <ExpenseFilter
-        onSelectCategory={category => setSelectedCategeory(category)}
+        onSelectCategory={category => setSelectedCategory(category)}
       />
       <ExpenseList expenses={visibleExpenses} onDelete={deleteExpense} />
     </>
